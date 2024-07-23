128a7d8a7878bbdb4b41a0d1d33a5511bfd44106
Priyanshu <priya14anshu@gmail.com>
2024-07-23 17:51:20 +0000

    global catch

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/health-checkup', (req, res) => {
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+
+    res.send("Your kidneys are " + kidneyLength + " long");
+});
+
+// Global error handler
+app.use((error, request, response, next) => {
+    console.error(error.stack);
+    response.status(500).json({
+        msg: "Something went wrong"
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000');
+});
